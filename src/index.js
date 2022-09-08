905a92c2f1e086b8b60ab45c16c9db2b9aab30eb
Svitlana Apolokhova <Svetlana.apolokhova@gmail.com>
2022-09-08 14:12:27 +0300

    search engine added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function formateDate(timestamp) {
   let data = new Date(timestamp);
-  console.log(data);
+
   let hours = data.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -30,15 +30,35 @@ function showTemperature(responce) {
   let cityElement = document.querySelector("#city");
   let conditionsElemet = document.querySelector("#conditions");
   let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(responce.data.main.temp);
   humidityElement.innerHTML = `Humidity: ${responce.data.main.humidity}`;
   conditionsElemet.innerHTML = responce.data.weather[0].main;
   windElement.innerHTML = `Wind: ${responce.data.wind.speed}`;
   cityElement.innerHTML = `${responce.data.name}, ${responce.data.sys.country}`;
   timeElement.innerHTML = formateDate(responce.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${responce.data.weather[0].main}@2x.png`
+  );
 }
-let keyApi = "00ceb4c1a676b6d772176beae74069d4";
-cityName = "Kharkiv";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keyApi}&units=metric`;
+function search(cityName) {
+  let keyApi = "00ceb4c1a676b6d772176beae74069d4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keyApi}&units=metric`;
 
-axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function searchHandale(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let submitElement = document.querySelector("#search-form");
+submitElement.addEventListener("submit", searchHandale);
